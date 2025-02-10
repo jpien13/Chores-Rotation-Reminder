6de476573aad0e52751afa252b9ca79d514e20fc
Jason Pien <jason_pien@brown.edu>
2025-02-10 15:30:18 -0500

    rotate on simulated days

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #from send import send_sms_via_email
 import argparse
 import json
+from datetime import datetime, time
+import pytz
 
 chore_list = [
     "Chore 0", # 0
@@ -39,20 +41,62 @@ def rotate_chores():
 
     write_json('roomate_to_chore.json', roommates)
 
+def check_days():
+    est = pytz.timezone('US/Eastern')
+    now = datetime.now(est)
+    
+    target_time = time(hour=15, minute=0)  # 15:00 = 3 PM
+    
+    # Get current weekday (0 = Monday, 6 = Sunday)
+    weekday = now.weekday()
+    current_time = now.time()
+    
+    is_sunday = weekday == 6 and current_time.hour == target_time.hour
+    is_monday = weekday == 0 and current_time.hour == target_time.hour
+    
+    return is_sunday, is_monday
+
 def main():
     parser = argparse.ArgumentParser(description='Script with two optional flags')
-    parser.add_argument('--print', action='store_true', help='print chores and roommates')
-    parser.add_argument('--rotate', action='store_true', help='rotate chores')
+    parser.add_argument('--testmode', action='store_true', help='test mode')
+    parser.add_argument('--prod', action='store_true', help='prod run')
     
     args = parser.parse_args()
-    
-    if args.print:
-        data = read_json('roomate_to_chore.json')
-        print(data)
-    if args.rotate:
-        rotate_chores()
-    if not (args.print or args.rotate):
+    if not (args.testmode or args.prod):
         print("No functions specified. Use --print or --rotate to run functions.")
+    
+    if args.testmode:
+
+        sim_day = input('Enter what day you are simulating. Enter anything if you just want to print the chore map: ').lower()
+        if sim_day == 'sunday':
+            is_sunday = True
+            is_monday = False
+        elif sim_day == 'monday':
+            is_sunday = False
+            is_monday = True
+        else:
+            data = read_json('roomate_to_chore.json')
+            print(data)
+            return
+        
+        if is_sunday:
+            data = read_json('roomate_to_chore.json')
+            print(data)
+        
+        if is_monday:
+            rotate_chores()
+
+    if args.prod:
+        is_sunday, is_monday = check_days()
+
+        if is_sunday:
+            data = read_json('roomate_to_chore.json')
+            print(data)
+        
+        if is_monday:
+            rotate_chores()
+
+    
 
 if __name__ == "__main__":
     
